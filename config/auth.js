446dcd2f278289380b150b1093e11e019cdcb750
Cory Asato <czasato@gmail.com>
2014-04-21 12:51:25 -1000

    hush

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   'googleAuth' : {
-    'clientID': '683443212417-l949gtsf8de7ghos2jp8oit5h2m0ba1l.apps.googleusercontent.com',
-    'clientSecret': '743bDhkYlW5IpDSeXK2JifgF',
+    'clientID': 'Your-Client-ID',
+    'clientSecret': 'Your-Psssst',
     'callbackURL': 'http://127.0.0.1:3000/auth/google/callback'
   }
   
